1c3d129cdcc5898a7f8ebecae20f87484c7b6b56
ivke995 <ivanivic842@gmail.com>
2026-02-25 23:35:13 +0100

    feat: adding to cart, add to address, prevent removing cart items after reload

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -47,7 +47,7 @@ export const AppContextProvider = (props) => {
       const { data } = await axios.get("/api/user/data", {
         headers: { Authorization: `Bearer ${token}` },
       });
-      
+
       if (data.success) {
         setUserData(data.user);
         setCartItems(data.user.cartItems);
@@ -67,7 +67,19 @@ export const AppContextProvider = (props) => {
       cartData[itemId] = 1;
     }
     setCartItems(cartData);
-    toast.success("Item added to cart");
+    if (user) {
+      try {
+        const token = await getToken();
+        await axios.post(
+          "/api/cart/update",
+          { cartData },
+          { headers: { Authorization: `Bearer ${token}` } },
+        );
+        toast.success("Item added to cart");
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
   };
 
   const updateCartQuantity = async (itemId, quantity) => {
@@ -78,6 +90,20 @@ export const AppContextProvider = (props) => {
       cartData[itemId] = quantity;
     }
     setCartItems(cartData);
+
+    if (user) {
+      try {
+        const token = await getToken();
+        await axios.post(
+          "/api/cart/update",
+          { cartData },
+          { headers: { Authorization: `Bearer ${token}` } },
+        );
+        toast.success("Cart updated");
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
   };
 
   const getCartCount = () => {
