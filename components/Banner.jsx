4779cabcf00d6f1f73234f332b998c70a4015dc8
Ivan Ivic <ivanivic842@gmail.com>
2026-03-14 08:32:16 +0100

    fix: banner content and images (#12)

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
+import Link from "next/link";
 
 const Banner = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between md:pl-20 py-14 md:py-0 bg-[#E6E9F2] my-16 rounded-xl overflow-hidden">
       <Image
         className="max-w-56"
-        src={assets.jbl_soundbox_image}
+        src={assets.header_slider_first}
         alt="jbl_soundbox_image"
       />
       <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
         <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
-          Unaprijedite svoje gejming iskustvo
+          Negujte kožu prirodno i nežno
         </h2>
         <p className="max-w-[343px] font-medium text-gray-800/60">
-          Od imerzivnog zvuka do preciznih kontrola — sve što vam treba za pobjedu
+          Melemi i prirodna kozmetika od pažljivo biranih sastojaka za svakodnevnu
+          negu.
         </p>
-        <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
+        <Link
+          href="/prodavnica"
+          className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white"
+        >
           Kupi sada
-          <Image className="group-hover:translate-x-1 transition" src={assets.arrow_icon_white} alt="arrow_icon_white" />
-        </button>
+          <Image
+            className="group-hover:translate-x-1 transition"
+            src={assets.arrow_icon_white}
+            alt="arrow_icon_white"
+          />
+        </Link>
       </div>
       <Image
         className="hidden md:block max-w-80"
-        src={assets.md_controller_image}
+        src={assets.header_slider_second}
         alt="md_controller_image"
       />
       <Image
         className="md:hidden"
-        src={assets.sm_controller_image}
+        src={assets.header_slider_second}
         alt="sm_controller_image"
       />
     </div>
